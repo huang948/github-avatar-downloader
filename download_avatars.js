1b3635b22c0df32107c941a801da853557b990be
Jordan Huang <jordanhuang99@gmail.com>
2018-05-14 23:47:03 +0000

    prints all the avatar url

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,26 @@
 var request = require('request');
+var token = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': "token " + token.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    var temp = JSON.parse(body);
+    cb(err, temp);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  for (var i = 0; i < result.length; i++) {
+    console.log("avatar_url: ", result[i]['avatar_url'])
+  }
+});
