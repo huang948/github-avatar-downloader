9b97700b9fa8e0ed9a1a66fbe628d7385d51139c
Jordan Huang <jordanhuang99@gmail.com>
2018-05-15 01:50:18 +0000

    caught all the errors with throw

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,22 @@
+require('dotenv').config();
 var args = process.argv.slice(2);
+if (args.length > 2) {
+  throw "Must have 2 parameters!";
+}
+
+
 var param1 = args[0];
 var param2 = args[1];
 if (args.length !== 2) {
   throw "Input exactly two parameters";
 }
 
-require('dotenv').config();
 
+if(process.env.GITHUB_TOKEN){
+  console.log("you can move on");
+} else {
+  throw "Where's the env file?!";
+}
 
 var request = require('request');
 var fs = require('fs');
@@ -14,7 +24,8 @@ var fs = require('fs');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
+  if (repoOwner && repoName) {
+    var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
@@ -25,7 +36,10 @@ function getRepoContributors(repoOwner, repoName, cb) {
   request(options, function(err, res, body) {
     var temp = JSON.parse(body);
     cb(err, temp);
-  });
+    });
+  } else {
+    throw "No repoOwner or no repoName";
+  }
 }
 
 getRepoContributors(param1, param2, function(err, result) {
@@ -38,6 +52,9 @@ getRepoContributors(param1, param2, function(err, result) {
 
 
 function downloadImageByURL(url, filePath) {
+  if (!fs.existsSync(filePath)) {
+    throw "The filePath does not exist";
+  }
   request.get(url)
   .on('error', function (err) {
     throw err;
